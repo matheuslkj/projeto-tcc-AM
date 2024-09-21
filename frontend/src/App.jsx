b8a9b0bedc4a39a97cf59387f86ffd1dccbcebff
alnsousa1 <alnprivado@gmail.com>
2024-09-21 10:12:03 -0300

    adicionando o navbar e components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-6xl mx-auto">
+       <nav className="rounded bg-indigo-900 text-white px-2 py-2.5 sm:px-4">
+    <div
+      className="container mx-auto flex flex-wrap items-center justify-between"
+    >
+      <a href="https://laraveller.com/" className="flex items-center">
+        Laraveller
+      </a>
+      <div
+        className="hidden w-full md:block md:w-auto"
+      >
+        <ul
+          className="
+            mt-4
+            flex flex-col
+            rounded-lg
+            p-4
+            md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium
+          "
+        >
+          <li>
+            <Link
+              to="/"
+              className="block rounded py-2 pr-4 pl-3 text-white"
+              aria-current="page"
+              >Home</Link>
+          </li>
+          <li>
+            <Link
+              to="/login"
+              className="block rounded py-2 pr-4 pl-3 text-white"
+              aria-current="page"
+              >Login</Link>
+          </li>
+          <li>
+            <Link
+              to="/register"
+              className="block rounded py-2 pr-4 pl-3 text-white"
+              aria-current="page"
+              >Registrar</Link>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  </nav>
+    </div>
   )
 }
 
