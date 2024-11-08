830fb939c0648990673b4ac803c3b2e0f5ced67d
alnsousa1 <alnprivado@gmail.com>
2024-11-08 09:41:57 -0300

    estilizando a fonte do AGENDAR e o icone do perfil

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
   const [userName, setUserName] = useState('');
   const navigate = useNavigate();
   const sidebarRef = useRef(null); // Referência para a sidebar
+  const dropdownRef = useRef(null); // Referência para o dropdown
 
   useEffect(() => {
     const storedUserName = localStorage.getItem('userName') || sessionStorage.getItem('userName');
@@ -53,6 +54,9 @@ const Navbar = () => {
       if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
         setSidebarOpen(false);
       }
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
     };
 
     document.addEventListener('mousedown', handleClickOutside);
@@ -69,17 +73,17 @@ const Navbar = () => {
           <FaBars />
         </button>
         <div className="text-xl font-bold"><Link to={"/"}>Logo</Link></div>
-        <div className="absolute right-20 font-bold"><Link to={"/agendamentos"}>Agendar</Link></div>
+        <div className="absolute right-20 mr-10 font-bold"><Link to={"/agendamentos"}>Agendar</Link></div>
         <div className="space-x-4 hidden md:flex items-center">
-          <div className="relative flex flex-col items-center">
+          <div className="relative flex flex-col items-center" ref={dropdownRef}>
             <button 
               onClick={toggleDropdown} 
               className="focus:outline-none text-white hover:text-blue-300 flex flex-col items-center"
             >
-              <FaUser className="text-1xl" />
+              <FaUser className="text-2xl" />
             </button>
             {userName && (
-              <div className="text-sm text-white mt-1">{userName}</div>
+              <div className="text-sm text-white mt-1 font-bold">{userName}</div>
             )}
             {dropdownOpen && (
               <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg py-2 z-50">
@@ -104,7 +108,7 @@ const Navbar = () => {
 
       {/* Sidebar */}
       <div
-        ref={sidebarRef} // Referência para a sidebar
+        ref={sidebarRef}
         className={`fixed top-0 left-0 w-64 h-full bg-gray-800 text-white transform ${
           sidebarOpen ? 'translate-x-0' : '-translate-x-full'
         } transition-transform duration-300 ease-in-out`}
